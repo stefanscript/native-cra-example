e6a0c8706cd24c24fb16cfc1bee46420ef78a25d
Marius Raducanu <marius_4896@yahoo.com>
2020-03-22 22:02:52 +0200

    fix on native screens

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import {useScreens} from 'react-native-screens';
-useScreens();
+import {enableScreens} from 'react-native-screens';
+enableScreens();
 
 AppRegistry.registerComponent(appName, () => App);
